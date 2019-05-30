2a571c36b8847b32aa053bda58180004d161e469
Daisuke Sato <daisukes@cmu.edu>
2019-05-30 13:34:44 -0400

    update

--- 02-run-cuda-installer.sh
+++ 02-run-cuda-installer.sh
@@ -1,5 +1,6 @@
 #!bin/sh
 
+sudo service lightdm stop
 mkdir -p temp
 if [ ! -e "temp/cuda10.run" ]; then
     wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux -O temp/cuda10.run
